c41514b3616f63d76937befc2bdfd6ab4c56d85a
Ayan Masood <ayan.m.dev@gmail.com>
2023-11-19 19:11:33 +0530

    Conversations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 export const metadata = {
   title: 'A-M-D Messenger',
@@ -14,8 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <AuthContext>
           <ToasterContext />
           {children}
+        </AuthContext>
       </body>
     </html>
   )
